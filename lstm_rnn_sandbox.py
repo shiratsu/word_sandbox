0019bbe167080d128e301f9afe0d0fe3d61a5b1a
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2017-04-23 15:27:59 +0900

    CharRNNがわかって来たよ

--- lstm_rnn_sandbox.py
+++ lstm_rnn_sandbox.py
@@ -1,5 +1,7 @@
 # coding: UTF-8
-
+import argparse
+import pickle
+import os
 import numpy as np
 from chainer import cuda, Variable, FunctionSet, optimizers
 import chainer.functions as F
@@ -12,14 +14,8 @@ mecab = MeCab.Tagger('mecabrc')
 
 step = 3
 maxlen = 20
-sentences = []
-nextWords = []
 aryWord = []
 
-word2id = {}
-id2word = {}
-
-model = Sequential()
 
 def get_words(strFile):
     '''
@@ -44,7 +40,8 @@ def get_words(strFile):
 # input data
 def makeDataSet():
 
-    words = list(aryWord)
+    vocab = {}
+    words = aryWord
     dataset = np.ndarray((len(words),), dtype=np.int32)
     for i, word in enumerate(words):
         if word not in vocab:
@@ -95,4 +92,42 @@ def makeHistoryWords():
 
 if __name__ == '__main__':
     argvs = sys.argv  # コマンドライン引数を格納したリストの取得
-    get_words(argvs[1])
+    get_words("kokoro_utf8.txt")
+    makeDataSet()
+
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir',                   type=str,   default='data/tinyshakespeare')
+    parser.add_argument('--checkpoint_dir',             type=str,   default='cv')
+    parser.add_argument('--gpu',                        type=int,   default=-1)
+    parser.add_argument('--rnn_size',                   type=int,   default=128)
+    parser.add_argument('--learning_rate',              type=float, default=2e-3)
+    parser.add_argument('--learning_rate_decay',        type=float, default=0.97)
+    parser.add_argument('--learning_rate_decay_after',  type=int,   default=10)
+    parser.add_argument('--decay_rate',                 type=float, default=0.95)
+    parser.add_argument('--dropout',                    type=float, default=0.0)
+    parser.add_argument('--seq_length',                 type=int,   default=50)
+    parser.add_argument('--batchsize',                  type=int,   default=50)
+    parser.add_argument('--epochs',                     type=int,   default=50)
+    parser.add_argument('--grad_clip',                  type=int,   default=5)
+    parser.add_argument('--init_from',                  type=str,   default='')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.checkpoint_dir):
+        os.mkdir(args.checkpoint_dir)
+
+    n_epochs    = args.epochs
+    n_units     = args.rnn_size
+    batchsize   = args.batchsize
+    bprop_len   = args.seq_length
+    grad_clip   = args.grad_clip
+
+    train_data, words, vocab = load_data(args)
+    pickle.dump(vocab, open('data/vocab.bin', 'wb'))
+    if args.gpu >= 0:
+        cuda.get_device(args.gpu).use()
+        model.to_gpu()
+
+    optimizer = optimizers.RMSprop(lr=args.learning_rate, alpha=args.decay_rate, eps=1e-8)
+    optimizer.setup(model)
