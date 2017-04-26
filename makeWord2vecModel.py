471d8ffebf49d50d5d6646e9b6105cf7be181fcd
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2017-04-27 08:12:39 +0900

    修正もれ

--- makeWord2vecModel.py
+++ makeWord2vecModel.py
@@ -7,8 +7,8 @@ import gensim
 import MeCab
 
 mecab = MeCab.Tagger('mecabrc')
+aryLine = []
 aryWord = []
-
 # テキストファイルがあるディレクトリ
 dir_dic = [
     "dokujo-tsushin",
@@ -37,16 +37,33 @@ def makeDicForFiles(files,dicname):
 
         while line:
             makeAryWord(line)
+            # aryLine.append(aryWord)
             line = f.readline()
+            # aryWord = []
 
         f.close
 
 # 配列作成
 def makeAryWord(line):
-    result = mecab.parse(line)
-    word = result[1:]
-    aryWord.append(word)
+    # result = mecab.parse(line)
+    # word = result[1:]
+    # aryWord.append(word)
+    #
+    # node = mecab.parseToNode(text)
+    # while node:
+    #     if node.feature.split(',')[0] == '名詞':
+    #         yield node.surface.lower()
+    #     node = node.next
+    for token in tokenize(line):
+        aryWord.append(token)
 
+def tokenize(text):
+    '''
+    とりあえず形態素解析して名詞だけ取り出す感じにしてる
+    '''
+    node = mecab.parseToNode(text)
+    while node:
+        node = node.next
 
 # モデルを作成して保存
 model = Word2Vec(aryWord)
